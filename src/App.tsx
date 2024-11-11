55992199f474ae2cbdedcc27df2f4610099b19dc
tiyoigusty <tiyooigustyy@gmail.com>
2024-11-11 17:01:40 +0700

    implement private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Outlet,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import Login from "./pages/login-page";
 import Register from "./pages/register-page";
 import Dashboard from "./pages/dashboard-page";
 
 function App() {
+  const PrivateRoute = () => {
+    const user = localStorage.getItem("user");
+
+    if (user) {
+      return <Outlet />;
+    } else {
+      return <Navigate to="/login" />;
+    }
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
