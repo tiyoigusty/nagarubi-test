c5da3060e4788a20e98ef332ba0fe8ca7bd0f13c
tiyoigusty <tiyooigustyy@gmail.com>
2024-11-11 16:40:27 +0700

    add color tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neon: "#39FF14",
+      },
+    },
   },
   plugins: [],
 };
